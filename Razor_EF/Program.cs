c1f1ffab85b0c5cfec083de0d9d2ffaf724f7f35
Sanya <kartuzovaleksandr@yandex.ru>
2025-10-23 19:22:57 +0300

    Использование статических файлов Предоставление каталога для просмотра - скачивания файлов

--- Razor_EF/Program.cs
+++ Razor_EF/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using Razor_EF.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,8 +25,23 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+// https://localhost:7151/Files/
+// даю каталог для скачивания
+app.UseFileServer(new FileServerOptions
+{
+    EnableDirectoryBrowsing = true,
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\Files")),
+    RequestPath = new PathString("/Files"),
+    EnableDefaultFiles = false
+});
+
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+
+// app.UseDirectoryBrowser(); // просмотр содержимое каталогов на сайте
+
+// app.UseDefaultFiles(); // переадресация на wwwroot
+
+app.UseStaticFiles(); // можно открывать полные адреса из wwwroot
 
 app.UseRouting();
 
